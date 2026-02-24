9d23bcb85f46df5134a8399dcc7d4b6a1684de64
stiven <johan.1701922249@ucaldas.edu.co>
2026-02-24 11:43:42 -0500

    add styles and theme configuration, create Home page and routing setup

---
+++ src/navigation/RootNavigator.tsx
@@ -0,0 +1,34 @@
+import React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+import HomeScreen from '../screens/HomeScreen';
+import theme from '../styles/theme';
+
+export type RootStackParamList = {
+  Home: undefined;
+};
+
+const Stack = createStackNavigator<RootStackParamList>();
+
+export default function RootNavigator() {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator
+        screenOptions={{
+          headerShown: false,
+          cardStyle: {
+            backgroundColor: theme.colors.white,
+          },
+        }}
+      >
+        <Stack.Screen
+          name="Home"
+          component={HomeScreen}
+          options={{
+            headerShown: false,
+          }}
+        />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+}
