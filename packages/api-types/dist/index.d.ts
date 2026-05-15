b1aa4e0ddf56c102278383919a67eaa381811421
J2517 <jackeline.rivera23296@ucaldas.edu.co>
2026-05-14 22:42:20 -0500

    remove dist

--- packages/api-types/dist/index.d.ts
+++
@@ -1,4 +0,0 @@
-export type { paths, components, operations } from './schema';
-export { ForoPreguntaSchema, ForoRespuestaSchema, EventoSchema, SesionEstudioSchema, SerieEstudioSchema, UsuarioSchema, GrupoSchema, MensajeSchema, MateriaSchema, EncuestaEstadoSchema, EncuestaTargetTypeSchema, EncuestaTargetSchema, EncuestaOpcionSchema, EncuestaSchema, } from './schemas';
-export type { Evento, Usuario, Grupo, Mensaje, Materia, EncuestaEstado, EncuestaTargetType, EncuestaTarget, EncuestaOpcion, Encuesta, ForoPregunta, ForoRespuesta, SesionEstudio, SerieEstudio, } from './schemas';
-//# sourceMappingURL=index.d.ts.map
